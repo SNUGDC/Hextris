fd4f465108ddaefbfc21b0834e5c496388914038
EQwon <bluetwintail@naver.com>
2016-12-30 13:27:56 +0900

    Section 할당 완료

--- Assets/Script/BaseController.cs
+++ Assets/Script/BaseController.cs
@@ -4,32 +4,12 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    public GameObject Tile;
-    public Transform Base;
-
-    private Vector3 XVector;
-    private Vector3 YVector;
-    private Vector3 ZVector;
-
-    void Awake()
-    {
-        XVector = new Vector3 (0, 2.1f, 0);
-        YVector = new Vector3 (1.83f, 1.05f, 0);
-        ZVector = new Vector3 (1.83f, -1.05f, 0);
-    }
-
-    void Start()
-    {
-        for (int i = 0; i < 7; i++)
-        {
-            for (int x = 0; x <= i; x++)
-            {
-                for (int y = 0; y <= i - x; y++)
-                {
-                    Vector3 Position = x * XVector + y * YVector + (i - x - y) * ZVector;
-                    Instantiate (Tile, Position, transform.rotation, Base);
-                }
-            }
-        }
-    }
+    public GameObject[] Section0;
+    public GameObject[] Section1;
+    public GameObject[] Section2;
+    public GameObject[] Section3;
+    public GameObject[] Section4;
+    public GameObject[] Section5;
+    public GameObject[] Section6;
+    public GameObject[] Section7;
 }
