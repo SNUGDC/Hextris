9ba55fe3d061b3fc721676a34743903868169780
EQwon <bluetwintail@naver.com>
2017-01-02 08:00:42 +0900

    Base가 돌아가게 만듬

--- Assets/Script/BaseController.cs
+++ Assets/Script/BaseController.cs
@@ -12,4 +12,72 @@ public class BaseController : MonoBehaviour
     public GameObject[] Section5;
     public GameObject[] Section6;
     public GameObject[] Section7;
+
+    private float GameTime;
+    private float RotateStartTime;
+    private float StartRotateAngle;
+    private bool IsRotateRight;
+    private bool IsRotateLeft;
+
+    private void Start()
+    {
+        GameTime = 0;
+        RotateStartTime = 0;
+        IsRotateRight = false;
+        IsRotateLeft = false;
+    }
+
+    private void Update()
+    {
+        GameTime = GameTime + Time.deltaTime;
+
+        RotateRight();
+        RotateLeft();
+    }
+
+    private void RotateRight()
+    {
+        if (Input.GetKeyUp(KeyCode.RightArrow) && (IsRotateRight == false) && (IsRotateLeft == false))
+        {
+            RotateStartTime = GameTime;
+            IsRotateRight = true;
+            StartRotateAngle = transform.eulerAngles.z;
+        }
+
+        if (IsRotateRight == true)
+        {
+            if ((GameTime - RotateStartTime) <= 1f)
+            {
+                transform.eulerAngles = new Vector3(0, 0, StartRotateAngle + (60f * (GameTime - RotateStartTime)));
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, StartRotateAngle + 60f);
+                IsRotateRight = false;
+            }
+        }
+    }
+
+    private void RotateLeft()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && (IsRotateLeft == false) && (IsRotateRight == false))
+        {
+            RotateStartTime = GameTime;
+            IsRotateLeft = true;
+            StartRotateAngle = transform.eulerAngles.z;
+        }
+
+        if (IsRotateLeft == true)
+        {
+            if ((GameTime - RotateStartTime) <= 1f)
+            {
+                transform.eulerAngles = new Vector3(0, 0, StartRotateAngle - (60f * (GameTime - RotateStartTime)));
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, StartRotateAngle - 60f);
+                IsRotateLeft = false;
+            }
+        }
+    }
 }
