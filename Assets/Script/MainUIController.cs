e57c5db59302787f194fd4e1b6812d77d76ddc02
EQwon <bluetwintail@naver.com>
2017-01-06 13:18:23 +0900

    Map Select 만드는 중

--- Assets/Script/MainUIController.cs
+++ Assets/Script/MainUIController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIController : MonoBehaviour
+public class MainUIController : MonoBehaviour
 {
     public Sprite[] Block;
     public GameObject Base;
---
+++ Assets/Script/StageUIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageUIController : MonoBehaviour
+{
+    public GameObject StageInfo;
+    public string[] StageName;
+
+    public void NextStage()
+    {
+        StageInfo.GetComponent<RectTransform>().position = new Vector3(StageInfo.GetComponent<RectTransform>().position.x - 1080, StageInfo.GetComponent<RectTransform>().position.y, 0);
+    }
+
+    public void PriorStage()
+    {
+        StageInfo.GetComponent<RectTransform>().position = new Vector3(StageInfo.GetComponent<RectTransform>().position.x + 1080, StageInfo.GetComponent<RectTransform>().position.y, 0);
+    }
+
+    public void GoToGameScene()
+    {
+
+    }
+}
