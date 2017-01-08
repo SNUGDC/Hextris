0527739ac39cf4fbd8b92bc9351d26543c1da168
EQwon <bluetwintail@naver.com>
2017-01-08 22:58:47 +0900

    스타트씬 만들고 맵선택 UI Scale 조정

---
+++ Assets/Script/StartController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+    public void MoveScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
