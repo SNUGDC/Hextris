ff46213948929b7dfbf7de0935b34d82c6a6ca7f
EQwon <bluetwintail@naver.com>
2017-01-10 20:12:31 +0900

    Stage 조절 설정

--- Assets/Script/StageUIController.cs
+++ Assets/Script/StageUIController.cs
@@ -8,14 +8,60 @@ public class StageUIController : MonoBehaviour
     public GameObject StageInfo;
     public string[] StageName;
 
+    private Vector2 NowRectPosition;
+    private float NowTime;
+    private float ButtonClickedTime;
+    private float FlowTime;
+    private bool GoToNextMap;
+    private bool GoToPriorMap;
+
+    private void Start()
+    {
+        NowTime = 0;
+        ButtonClickedTime = 0;
+        FlowTime = 0;
+        GoToNextMap = false;
+        GoToPriorMap = false;
+    }
+
+    private void Update()
+    {
+        NowTime = NowTime + Time.deltaTime;
+        FlowTime = NowTime - ButtonClickedTime;
+        Debug.Log (FlowTime);
+
+        if (GoToNextMap == true)
+        {
+            StageInfo.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (StageInfo.GetComponent<RectTransform> ().anchoredPosition.x - 49 * FlowTime, StageInfo.GetComponent<RectTransform> ().anchoredPosition.y);
+            if (FlowTime >= 1f)
+            {
+                GoToNextMap = false;
+                StageInfo.GetComponent<RectTransform> ().anchoredPosition = NowRectPosition - new Vector2 (1080, 0);
+            }
+        }
+        else if (GoToPriorMap == true)
+        {
+            StageInfo.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (StageInfo.GetComponent<RectTransform> ().anchoredPosition.x + 49 * FlowTime, StageInfo.GetComponent<RectTransform> ().anchoredPosition.y);
+            if (FlowTime >= 1f)
+            {
+                GoToPriorMap = false;
+                StageInfo.GetComponent<RectTransform> ().anchoredPosition = NowRectPosition + new Vector2 (1080, 0);
+            }
+        }
+    }
+
     public void NextStage()
     {
-        StageInfo.GetComponent<RectTransform>().anchoredPosition = new Vector2(StageInfo.GetComponent<RectTransform>().anchoredPosition.x - 1080, StageInfo.GetComponent<RectTransform>().anchoredPosition.y);
+        ButtonClickedTime = NowTime;
+        GoToNextMap = true;
+        NowRectPosition = StageInfo.GetComponent<RectTransform> ().anchoredPosition;
     }
 
     public void PriorStage()
     {
-        StageInfo.GetComponent<RectTransform>().anchoredPosition = new Vector2(StageInfo.GetComponent<RectTransform>().anchoredPosition.x + 1080, StageInfo.GetComponent<RectTransform>().anchoredPosition.y);
+        ButtonClickedTime = NowTime;
+        GoToPriorMap = true;
+        NowRectPosition = StageInfo.GetComponent<RectTransform> ().anchoredPosition;
     }
 
     public void GoToGameScene(string SceneName)
