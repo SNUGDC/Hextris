649175982394832ff2e8ae90b51338dc42b08c3f
EQwon <bluetwintail@naver.com>
2017-01-11 09:33:42 +0900

    Pause UI 수정중

--- Assets/Script/MainUIController.cs
+++ Assets/Script/MainUIController.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class MainUIController : MonoBehaviour
 {
-    public Sprite[] Block;    
+    public Sprite[] Block;
+    public Sprite SwipeSensitivityButtonImage;
+    public Sprite CreateBlockSensitivityButtonImage;
 
     private GameObject Base;
     private GameObject PausePanel;
-    private GameObject SwipeSensitivityButton;
-    private GameObject CreateBlockSensitivityButton;
+    private Image[] SwipeSensitivityButton;
+    private Image[] CreateBlockSensitivityButton;
 
     private Image[] NextBlockImage;
     private int[] BlockOrder;
@@ -23,8 +25,24 @@ public class MainUIController : MonoBehaviour
         NextBlockImage[2] = GameObject.Find("Next Block 3").GetComponent<Image>();
         Base = GameObject.Find("Base");
         PausePanel = GameObject.Find("Pause Panel");
-        SwipeSensitivityButton = GameObject.Find("Swipe Sensitivity Button");
-        CreateBlockSensitivityButton = GameObject.Find("Create Block Sensitivity Button");
+
+        SwipeSensitivityButton = new Image[7];
+        CreateBlockSensitivityButton = new Image[7];
+        SwipeSensitivityButton[0] = GameObject.Find("Swipe Sensitivity = 1").GetComponent<Image>();
+        SwipeSensitivityButton[1] = GameObject.Find("Swipe Sensitivity = 2").GetComponent<Image>();
+        SwipeSensitivityButton[2] = GameObject.Find("Swipe Sensitivity = 3").GetComponent<Image>();
+        SwipeSensitivityButton[3] = GameObject.Find("Swipe Sensitivity = 4").GetComponent<Image>();
+        SwipeSensitivityButton[4] = GameObject.Find("Swipe Sensitivity = 5").GetComponent<Image>();
+        SwipeSensitivityButton[5] = GameObject.Find("Swipe Sensitivity = 6").GetComponent<Image>();
+        SwipeSensitivityButton[6] = GameObject.Find("Swipe Sensitivity = 7").GetComponent<Image>();
+        CreateBlockSensitivityButton[0] = GameObject.Find("Create Block Sensitivity = 1").GetComponent<Image>();
+        CreateBlockSensitivityButton[1] = GameObject.Find("Create Block Sensitivity = 2").GetComponent<Image>();
+        CreateBlockSensitivityButton[2] = GameObject.Find("Create Block Sensitivity = 3").GetComponent<Image>();
+        CreateBlockSensitivityButton[3] = GameObject.Find("Create Block Sensitivity = 4").GetComponent<Image>();
+        CreateBlockSensitivityButton[4] = GameObject.Find("Create Block Sensitivity = 5").GetComponent<Image>();
+        CreateBlockSensitivityButton[5] = GameObject.Find("Create Block Sensitivity = 6").GetComponent<Image>();
+        CreateBlockSensitivityButton[6] = GameObject.Find("Create Block Sensitivity = 7").GetComponent<Image>();
+
 
         BlockOrder = new int[3];
         PausePanel.SetActive(false);
@@ -44,8 +62,18 @@ public class MainUIController : MonoBehaviour
     {
         PausePanel.SetActive (true);
         PlayerPrefs.SetString("In Game State", "Pause");
-        SwipeSensitivityButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(((PlayerPrefs.GetFloat("Swipe Sensitivity") - 60) * 1.2f) - 290, SwipeSensitivityButton.GetComponent<RectTransform>().anchoredPosition.y);
-        CreateBlockSensitivityButton.GetComponent<RectTransform>().anchoredPosition = new Vector2((PlayerPrefs.GetFloat("Create Block Sensitivity") * 10f) - 290, CreateBlockSensitivityButton.GetComponent<RectTransform>().anchoredPosition.y);
+
+        for (int i = 0; i < 7; i++)
+        {
+            SwipeSensitivityButton[i].color = new Color(1, 1, 1, 0);
+        }
+        SwipeSensitivityButton[(int)(PlayerPrefs.GetFloat("Swipe Sensitivity") / 70) - 1].color = new Color(1, 1, 1, 1);
+
+        for (int i = 0; i < 7; i++)
+        {
+            CreateBlockSensitivityButton[i].color = new Color(1, 1, 1, 0);
+        }
+        CreateBlockSensitivityButton[(int)(PlayerPrefs.GetFloat("Create Block Sensitivity") / 10)].color = new Color(1, 1, 1, 1);
     }
 
     public void PlayAgain()
@@ -54,21 +82,25 @@ public class MainUIController : MonoBehaviour
         PlayerPrefs.SetString("In Game State", "Play");
     }
 
-    public void SwipeSensitivityButtonControl()
+    public void SwipeSensitivityButtonControl(int Sensitivity)
     {
-        if (Input.mousePosition.x >= 260 && Input.mousePosition.x <= 830)
+        for (int i = 0; i < 7; i++)
         {
-            SwipeSensitivityButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(Input.mousePosition.x - 550, SwipeSensitivityButton.GetComponent<RectTransform>().anchoredPosition.y);
-            PlayerPrefs.SetFloat("Swipe Sensitivity", (float)((Input.mousePosition.x - 260) / 1.2) + 60);
+            SwipeSensitivityButton[i].color = new Color(1, 1, 1, 0);
         }
+
+        SwipeSensitivityButton[Sensitivity].color = new Color(1, 1, 1, 1);
+        PlayerPrefs.SetFloat("Swipe Sensitivity", Sensitivity * 70 + 70);
     }
 
-    public void BlockCreateSensitivityButtonControl()
+    public void BlockCreateSensitivityButtonControl(int Sensitivity)
     {
-        if (Input.mousePosition.x >= 260 && Input.mousePosition.x <= 830)
+        for (int i = 0; i < 7; i++)
         {
-            CreateBlockSensitivityButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(Input.mousePosition.x - 550, CreateBlockSensitivityButton.GetComponent<RectTransform>().anchoredPosition.y);
-            PlayerPrefs.SetFloat("Create Block Sensitivity", (float)((Input.mousePosition.x - 260) / 10));
+            CreateBlockSensitivityButton[i].color = new Color(1, 1, 1, 0);
         }
+
+        CreateBlockSensitivityButton[Sensitivity].color = new Color(1, 1, 1, 1);
+        PlayerPrefs.SetFloat("Create Block Sensitivity", Sensitivity * 10);
     }
 }
