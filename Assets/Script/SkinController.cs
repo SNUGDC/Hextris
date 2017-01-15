6a8a5e473da63c1c7e473821515a4ab4b4bae0e6
EQwon <bluetwintail@naver.com>
2017-01-16 07:47:24 +0900

    Skin Scene을 코드로 관리하자

---
+++ Assets/Script/SkinController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinController : MonoBehaviour
+{
+    public Image SkinName;
+    public Image BlueTile;
+    public Image RedTile;
+    public Image YellowTile;
+    public GameObject CheckMark;
+
+    public Sprite[] SkinNameWord;
+    public Sprite[] SkinBlue;
+    public Sprite[] SkinRed;
+    public Sprite[] SkinYellow;
+}
