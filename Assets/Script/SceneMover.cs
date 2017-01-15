2b9020a486d8fa5c4a608751864a9aa323639dc5
EQwon <bluetwintail@naver.com>
2017-01-16 04:16:06 +0900

    필요없는거 다 지우고 이름 통일

--- Assets/Script/SceneMover.cs
+++ Assets/Script/SceneMover.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartController : MonoBehaviour
+public class SceneMover : MonoBehaviour
 {
     public void MoveScene(string SceneName)
     {
