3a5ea67b87f8ea2bb2bcc43856ce42c8827dbd3c
EQwon <bluetwintail@naver.com>
2017-01-16 18:51:04 +0900

    스킨 화면 넘어가는 코드는 짬

--- Assets/Script/SkinController.cs
+++ Assets/Script/SkinController.cs
@@ -15,4 +15,47 @@ public class SkinController : MonoBehaviour
     public Sprite[] SkinBlue;
     public Sprite[] SkinRed;
     public Sprite[] SkinYellow;
+
+    private int SkinNumber;
+    private int WhatSkinShow;
+
+    private void Start()
+    {
+        SkinNumber = PlayerPrefs.GetInt("Skin Number");
+        WhatSkinShow = 0;
+        CheckMark.SetActive(false);
+    }
+
+    private void Update()
+    {
+        SkinName.sprite = SkinNameWord[WhatSkinShow];
+        BlueTile.sprite = SkinBlue[WhatSkinShow];
+        RedTile.sprite = SkinRed[WhatSkinShow];
+        YellowTile.sprite = SkinYellow[WhatSkinShow];
+
+        if (WhatSkinShow == SkinNumber)
+        {
+            CheckMark.SetActive(true);
+        }
+        else
+        {
+            CheckMark.SetActive(false);
+        }
+    }
+
+    public void NextSkin()
+    {
+        if (WhatSkinShow + 1 < SkinNameWord.Length)
+        {
+            WhatSkinShow = WhatSkinShow + 1;
+        }
+    }
+
+    public void PriorSkin()
+    {
+        if (WhatSkinShow - 1 >= 0)
+        {
+            WhatSkinShow = WhatSkinShow - 1;
+        }
+    }
 }
