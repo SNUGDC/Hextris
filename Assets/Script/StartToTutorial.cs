dc21d8d2fe9ad0f565d7c7a3045b30ffeda801cf
EQwon <bluetwintail@naver.com>
2017-01-21 03:34:03 +0900

    튜토리얼 제작중

---
+++ Assets/Script/StartToTutorial.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartToTutorial : MonoBehaviour
+{
+    public Image YesButtonImage;
+    public Image NoButtonImage;
+    public Image NeverButtonImage;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetString("Tutorial Panel") == "Off")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void PushYesButton()
+    {
+        YesButtonImage.color = new Color(1, 1, 1, 1);
+    }
+
+    public void PushNoButton()
+    {
+        NoButtonImage.color = new Color(1, 1, 1, 1);
+        gameObject.SetActive(false);
+    }
+
+    public void PushNeverButton()
+    {
+        NeverButtonImage.color = new Color(1, 1, 1, 1);
+        PlayerPrefs.SetString("Tutorial Panel", "Off");
+    }
+}
