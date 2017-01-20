d064c3435b4314f6851cbe8b098534c16e8deb39
EQwon <bluetwintail@naver.com>
2017-01-21 07:38:36 +0900

    일단 How To Play 완성한듯

---
+++ Assets/Script/StartSceneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneController : MonoBehaviour
+{
+    public GameObject HowToPlayPanel;
+    public GameObject HowToPlay1;
+    public GameObject HowToPlay2;
+
+    private void Start()
+    {
+        HowToPlay1.SetActive(true);
+        HowToPlay2.SetActive(false);
+        HowToPlayPanel.SetActive(false);
+    }
+
+    private void Update()
+    { }
+
+    public void Next()
+    {
+        HowToPlay1.SetActive(false);
+        HowToPlay2.SetActive(true);
+    }
+
+    public void Previous()
+    {
+        HowToPlay1.SetActive(true);
+        HowToPlay2.SetActive(false);
+    }
+}
