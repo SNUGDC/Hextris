9164a9012e9bdd1c72d390f276e13cb5f3baaa01
EQwon <bluetwintail@naver.com>
2017-02-02 19:37:28 +0900

    Revert "Best Time 만드는 중"
    This reverts commit 0aac1b4dad99e755528fea1f14aeecd6f4991642.

--- Assets/Script/PlayTimeController.cs
+++ Assets/Script/PlayTimeController.cs
@@ -7,42 +7,17 @@ public class PlayTimeController : MonoBehaviour
 {
     public Sprite[] Number;
     public Image[] TimeImage;
-    public Image[] BestTimeImage;
 
     private GameObject Base;
     private float GameTime_float;
-    private float BestTime_float;
     private int GameTime_int;
-    private int BestTime_int;
     private int[] PlayTimeNumber;
-    private int[] BestTimeNumber;
 
     private void Start()
     {
         Base = GameObject.Find ("Base");
 
         PlayTimeNumber = new int[3];
-        BestTimeNumber = new int[3];
-
-        BestTime_float = PlayerPrefs.
-
-        GameTime_int = (int)GameTime_float;
-        PlayTimeNumber[0] = GameTime_int / 60;
-
-        GameTime_int = GameTime_int - PlayTimeNumber[0] * 60;
-        PlayTimeNumber[1] = GameTime_int / 10;
-        PlayTimeNumber[2] = GameTime_int % 10;
-
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 10; j++)
-            {
-                if (PlayTimeNumber[i] == j)
-                {
-                    TimeImage[i].sprite = Number[j];
-                }
-            }
-        }
     }
 
     private void Update()
